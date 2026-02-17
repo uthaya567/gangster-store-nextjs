522c1c7d54329adff222d9748ff5d8e25d951dfe
Asta96 <kumaruthaya567@example.com>
2026-02-17 16:07:52 +0530

    Updated homepage to server redirect to men

--- app/page.tsx
+++ app/page.tsx
@@ -1,14 +1,6 @@
 "use client";
-
-import { useEffect } from "react";
-import { useRouter } from "next/navigation";
+import { redirect } from "next/navigation";
 
 export default function Page() {
-  const router = useRouter();
-
-  useEffect(() => {
-    router.replace("/men");   // replace is better for redirects
-  }, [router]);
-
-  return null;
+  redirect("/men");
 }
